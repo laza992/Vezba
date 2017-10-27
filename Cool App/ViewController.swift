1d3446a9db8ba966ff36c7f235b8af0387e3d70b
Laza <laza@laza.local>
2017-10-27 14:22:21 +0200

    My message

--- Cool App/ViewController.swift
+++ Cool App/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Boom: UIImageView!
+    @IBOutlet weak var back: UIImageView!
+    @IBOutlet weak var butt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makebecool(_ sender: Any) {
+        Boom.isHidden = false
+        back.isHidden = false
+        butt.isHidden = true
+    }
+    
 
 }
 
